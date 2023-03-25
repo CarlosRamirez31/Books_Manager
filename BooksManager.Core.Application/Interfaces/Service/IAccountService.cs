1d1d181fb03ae8a3d9829fc173964c31d954d2e9
CarlosRamirez31 <cr2503150@gmail.com>
2023-03-25 00:21:31 -0400

    Methods to register, authenticate, general token and refresh token added

---
+++ BooksManager.Core.Application/Interfaces/Service/IAccountService.cs
@@ -0,0 +1,11 @@
+﻿using Application.Wrappers;
+using BooksManager.Core.Application.Dtos.Users;
+
+namespace BooksManager.Core.Application.Interfaces.Service
+{
+    public interface IAccountService
+    {
+        Task<Response<AuthenticationResponse>> AuthenticateAsync(AuthenticationRequest request, string ipAddress);
+        Task<Response<string>> RegisterAsync(RegisterRequest request, string origin);
+    }
+}
---
+++ BooksManager.Infrastructure.Identity/Services/AccountService.cs
@@ -0,0 +1,145 @@
+﻿using Application.Exceptions;
+using Application.Wrappers;
+using BooksManager.Core.Application.Dtos.Users;
+using BooksManager.Core.Application.Enums;
+using BooksManager.Core.Application.Interfaces.Service;
+using BooksManager.Core.Domain.Setting;
+using BooksManager.Infrastructure.Identity.Helper;
+using BooksManager.Infrastructure.Identity.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace BooksManager.Infrastructure.Identity.Services
+{
+    public class AccountService : IAccountService
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly JWTSettings _jwtSettings;
+
+        public AccountService(UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager, SignInManager<ApplicationUser> signInManager, JWTSettings jwtSettings)
+        {
+            _userManager = userManager;
+            _roleManager = roleManager;
+            _signInManager = signInManager;
+            _jwtSettings = jwtSettings;
+        }
+
+        public async Task<Response<string>> RegisterAsync(RegisterRequest request, string origin)
+        {
+            var user = await _userManager.FindByNameAsync(request.UserName);
+            if (user is null)
+                throw new ApiException($"El nombre de usuario {request.UserName} ya ha sido registrado previamente");
+
+            user = await _userManager.FindByEmailAsync(request.Email);
+            if (user is null)
+                throw new ApiException($"El correo electronico {request.Email} ya ha sido registrado previamente");
+
+            user = new ApplicationUser
+            {
+                Email = request.Email,
+                FirstName = request.FirstName,
+                LastName = request.LastName,
+                UserName = request.UserName,
+                EmailConfirmed = true,
+                PhoneNumberConfirmed = true
+            };
+
+            var result = await _userManager.CreateAsync(user, request.Password);
+            if (!result.Succeeded)
+                throw new ApiException($"Errores {result.Errors}");
+
+            await _userManager.AddToRoleAsync(user, Roles.Basic.ToString());
+            return new Response<string>(user.Id, message: $"Usuario registrado exitosamente {user.UserName}");
+        }
+
+        public async Task<Response<AuthenticationResponse>> AuthenticateAsync(AuthenticationRequest request, string ipAddress)
+        {
+            var user = await _userManager.FindByEmailAsync(request.Email);
+            if (user is null)
+                throw new ApiException($"No hay una cuenta registrada el email {request.Email}");
+
+            var result = await _signInManager.PasswordSignInAsync(user, request.Password, false, lockoutOnFailure: false);
+            if (!result.Succeeded)
+                throw new ApiException("Las credenciales de usuario no son validas");
+
+            JwtSecurityToken jwtSecurityToken = await GenerateJWTToken(user);
+
+            AuthenticationResponse response = new AuthenticationResponse();
+            response.Id = user.Id;
+            response.JWToken = new JwtSecurityTokenHandler().WriteToken(jwtSecurityToken);
+            response.Email = user.Email;
+            response.UserName = user.UserName;
+
+            var rolesList = await _userManager.GetRolesAsync(user);
+            response.Roles = rolesList.ToList();
+            response.IsVerified = user.EmailConfirmed;
+
+            var refreshToken = GenerateRefreshToken(ipAddress);
+            response.RefreshToken = refreshToken.Token;
+            return new Response<AuthenticationResponse>(response, message: $"Usuario autenticado {user.Id}");
+        }
+
+        private async Task<JwtSecurityToken> GenerateJWTToken(ApplicationUser user)
+        {
+            var userClaims = await _userManager.GetClaimsAsync(user);
+            var roles = await _userManager.GetRolesAsync(user);
+
+            var roleClaims = new List<Claim>();
+            foreach (var role in roles)
+            {
+                roleClaims.Add(new Claim("roles", role));
+            }
+
+            string ipAddress = IpHelper.GetIpAddress();
+
+            var claims = new[]
+            {
+                new Claim(JwtRegisteredClaimNames.Sub, user.UserName),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                new Claim(JwtRegisteredClaimNames.Email, user.Email),
+                new Claim("uid", user.Id),
+                new Claim("ip", ipAddress)
+            }
+            .Union(userClaims)
+            .Union(roleClaims);
+
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_jwtSettings.Key));
+            var signingCredential = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+            var jwtSecurityToken = new JwtSecurityToken(
+                issuer: _jwtSettings.Issuer,
+                audience: _jwtSettings.Audience,
+                claims: claims,
+                expires: DateTime.Now.AddMinutes(_jwtSettings.DurationInMinute),
+                signingCredentials: signingCredential
+                );
+
+            return jwtSecurityToken;
+        }
+
+        private RefreshToken GenerateRefreshToken(string ipAddress)
+        {
+            return new RefreshToken
+            {
+                Token = RandomTokenString(),
+                Expires = DateTime.Now.AddDays(4),
+                Created = DateTime.Now,
+                CreateByIp = ipAddress
+            };
+        }
+
+        private string RandomTokenString()
+        {
+            using var rngCryptoServiceProvider = new RNGCryptoServiceProvider();
+            var randomByte = new Byte[40];
+            rngCryptoServiceProvider.GetBytes(randomByte);
+            return BitConverter.ToString(randomByte).Replace("-", "");
+        }
+    }
+}
